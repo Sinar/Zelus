e7e27830db406be13bc3be5c6dce6350b801f077
Chang Sau Sheong <sausheong@gmail.com>
2012-07-22 14:25:46 +0800

    minor tweaks for Heroku update

--- migrations.rb
+++ migrations.rb
@@ -45,8 +45,8 @@ module Setup
       party.code = rec[0]
       party.name = rec[1]
       party.name_in_malay = rec[2]
-      party.founded_in = rec[3].to_i unless rec[3].nil?      
-      party.disbanded_in = rec[4].to_i unless rec[4].nil?      
+      party.founded_in = rec[3]
+      party.disbanded_in = rec[4]
       party.save      
     end    
   end
@@ -58,8 +58,8 @@ module Setup
         member = Person.first :uuid => rec[1]
         party = Party.first :uuid => rec[6]
         m = Membership.create :person => member, :party => party
-        m.joined_at = rec[2].to_i unless rec[2].nil?
-        m.parted_at = rec[3].to_i unless rec[3].nil?
+        m.joined_at = rec[2]
+        m.parted_at = rec[3]
         m.save
       rescue
         p $!
@@ -75,8 +75,8 @@ module Setup
       coalition.code = rec[0]
       coalition.name = rec[1]
       coalition.name_in_malay = rec[2]
-      coalition.founded_in = rec[3].to_i unless rec[3].nil?      
-      coalition.disbanded_in = rec[4].to_i unless rec[4].nil?      
+      coalition.founded_in = rec[3]
+      coalition.disbanded_in = rec[4]
       coalition.save      
     end    
   end
@@ -88,8 +88,8 @@ module Setup
         coalition = Coalition.first :uuid => rec[6]
         party = Party.first :uuid => rec[5]
         c = Coalitionship.create :coalition => coalition, :party => party
-        c.joined_at = rec[1].to_i unless rec[1].nil?
-        c.parted_at = rec[2].to_i unless rec[2].nil?
+        c.joined_at = rec[1]
+        c.parted_at = rec[2]
         c.save
       rescue
         p $!
