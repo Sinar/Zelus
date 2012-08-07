697d8f0b9f8048705a1458f915034c5032e36092
yclian <foss@yclian.com>
2012-08-07 09:32:30 +0800

    Fix Factory's factories to generate UUID and timestamp by calling custom constroctor of DM classes. [#33764757]

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,12 @@
 FactoryGirl.define do
 
-  factory :person do
+  factory :person do |t|
+    initialize_with { Person.create }
     sequence(:name) { |n| "Nicole Neal (#{n})" }
-    factory :person_with_party do
-      after(:create) do |person|
-        # TODO
-        # party = FactoryGirl.create(:party)
-        # FactoryGirl.create(:party_membership, { party: party, member: member, joined_at: 1.year.ago.year })
-      end
-    end
   end
 
   factory :user do
+    initialize_with { User.create }
     sequence(:name) { |n| "user-#{n}" }
     after(:build) do |u|
       u.generate_api_key
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'factory_girl'
-require 'factories'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f }
+require 'factories'
+
+FactoryGirl.duplicate_attribute_assignment_from_initialize_with = false
 
 RSpec.configure do |c|
   c.include FactoryGirl::Syntax::Methods
